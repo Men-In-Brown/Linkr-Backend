f86c5e78fa8c2fa380aa6b556f0d090d2f817d64
nVarap <nvarap@gmail.com>
2024-01-26 11:57:38 -0800

    First commit to branch, should commit to test-dev. Admin panel setup

---
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/Linkr.java
@@ -0,0 +1,17 @@
+package com.nighthawk.spring_portfolio.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Linkr {
+    @GetMapping("/linkr")
+    // @RequestParam handles variables binding to frontend, defaults, etc
+    public String linkrdisplay() {
+        
+        
+        return "linkradmin";
+    }
+}
