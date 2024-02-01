8422098ca16d93cae39ad412e2953ce559a1d457
nVarap <nvarap@gmail.com>
2024-02-01 12:09:39 -0800

    Committing early changes - basic JWT addons, should not break #2

--- src/main/java/com/nighthawk/spring_portfolio/controllers/Linkr/LinkrSignin.java
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/Linkr/LinkrSignin.java
@@ -1,19 +1,19 @@
-package com.nighthawk.spring_portfolio.controllers;
+package com.nighthawk.spring_portfolio.controllers.Linkr;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @Controller
-public class Linkr {
+public class LinkrSignin {
     @GetMapping("/linkr")
     // @RequestParam handles variables binding to frontend, defaults, etc
     public String linkrdisplay() {
-        
-        
         return "linkradmin";
     }
+
 
     
 
