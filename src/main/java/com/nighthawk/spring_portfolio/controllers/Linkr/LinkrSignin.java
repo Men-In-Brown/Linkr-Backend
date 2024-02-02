54bc3b6bd2f7449b830de6e97bbefc056127266b
nVarap <nvarap@gmail.com>
2024-02-02 12:33:21 -0800

    Added Linkr PAT issues

--- src/main/java/com/nighthawk/spring_portfolio/controllers/Linkr/LinkrSignin.java
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/Linkr/LinkrSignin.java
@@ -1,5 +1,6 @@
 package com.nighthawk.spring_portfolio.controllers.Linkr;
 
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
