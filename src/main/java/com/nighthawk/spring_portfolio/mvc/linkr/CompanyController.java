f1f4f3b44bd531b6adecdef736978bf676b375a7
tanayp327 <tanaypatel327@gmail.com>
2024-02-07 22:37:47 -0800

    adding DTO for serialization issue

--- src/main/java/com/nighthawk/spring_portfolio/mvc/linkr/CompanyController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/linkr/CompanyController.java
@@ -1,45 +1,68 @@
 package com.nighthawk.spring_portfolio.mvc.linkr;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.modelmapper.ModelMapper;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/companies")
 public class CompanyController {
 
     private final CompanyService companyService;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public CompanyController(CompanyService companyService) {
+    public CompanyController(CompanyService companyService, ModelMapper modelMapper) {
         this.companyService = companyService;
+        this.modelMapper = modelMapper;
     }
 
     @GetMapping
-    public ResponseEntity<List<Company>> getAllCompanies() {
+    public ResponseEntity<List<CompanyDTO>> getAllCompanies() {
         List<Company> companies = companyService.getAllCompanies();
-        return new ResponseEntity<>(companies, HttpStatus.OK);
+        List<CompanyDTO> companyDTOs = companies.stream()
+                .map(company -> modelMapper.map(company, CompanyDTO.class))
+                .collect(Collectors.toList());
+
+        // Log the number of companies retrieved
+        log.info("Retrieved {} companies", companyDTOs.size());
+
+        return new ResponseEntity<>(companyDTOs, HttpStatus.OK);
     }
 
     @GetMapping("/{companyId}")
     public ResponseEntity<Company> getCompanyById(@PathVariable Long companyId) {
+        log.info("Attempting to retrieve company with ID: {}", companyId);
         Optional<Company> company = companyService.getCompanyById(companyId);
-        return company.map(value -> ResponseEntity.ok().body(value))
-                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+        if (company.isPresent()) {
+            log.info("Found company with ID: {}", companyId);
+            return ResponseEntity.ok().body(company.get());
+        } else {
+            log.warn("Company with ID {} not found", companyId);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 
     @PostMapping
     public ResponseEntity<Company> addCompany(@RequestBody Company company) {
+        log.info("Attempting to add company: {}", company);
         Company addedCompany = companyService.createCompany(company);
+        log.info("Company added successfully: {}", addedCompany);
         return new ResponseEntity<>(addedCompany, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{companyId}")
     public ResponseEntity<Void> deleteCompany(@PathVariable Long companyId) {
+        log.info("Attempting to delete company with ID: {}", companyId);
         companyService.deleteCompany(companyId);
+        log.info("Company with ID {} deleted successfully", companyId);
         return ResponseEntity.noContent().build();
     }
 }
